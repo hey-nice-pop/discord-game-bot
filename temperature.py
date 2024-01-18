744315d307c372ea98adf468bd3aa93fef242110
hey-nice-pop <imo_watch@icloud.com>
2024-01-18 15:00:20 +0900

    特定チャンネルで温度を通知するように変更

--- temperature.py
+++ temperature.py
@@ -3,18 +3,24 @@ import datetime
 from discord import Message
 from filelock import FileLock, Timeout
 
+# 温度を送信するチャンネル
+TARGET_THREAD_CHANNEL_ID = 1184679498222932049
+
 # JSONファイルのパス
 JSON_FILE_PATH = 'temperature.json'
 LOCK_PATH = 'temperature.json.lock'
 
 async def process_message(message: Message):
+    # 対象のスレッドを取得
+    target_thread = message.guild.get_channel(TARGET_THREAD_CHANNEL_ID)
+
     try:
         with FileLock(LOCK_PATH, timeout=5):
             data, new_file_created = load_json()
             
             # JSONファイルが新しく作成された場合に60度の通知を送信
             if new_file_created:
-                await message.channel.send('60度に達しました')
+                await target_thread.send('60度に達しました')
 
             # 現在の日付を取得
             today = str(datetime.date.today())
@@ -25,7 +31,7 @@ async def process_message(message: Message):
                 data['message_count'] = 0
                 reset_temperature(data)
                 data['last_date'] = today
-                await message.channel.send('60度に達しました')
+                await target_thread.send('60度に達しました')
 
             previous_temperature = data['temperature']
 
@@ -46,6 +52,9 @@ async def process_message(message: Message):
         # 必要に応じて、ここで再試行のロジックを追加する
 
 async def check_temperature_thresholds(message: Message, data: dict, previous_temperature: float):
+    # 対象のスレッドを取得
+    target_thread = message.guild.get_channel(TARGET_THREAD_CHANNEL_ID)
+
     thresholds = [70, 80, 90]
     for threshold in thresholds:
         if previous_temperature < threshold <= data['temperature']:
@@ -54,18 +63,21 @@ async def check_temperature_thresholds(message: Message, data: dict, previous_te
                 await handle_90_degree_threshold(data, message)
             else:
                 # それ以外の閾値を超えた場合、通知メッセージを送信
-                await message.channel.send(f'{threshold}度を超えました')
+                await target_thread.send(f'{threshold}度を超えました')
 
 async def handle_90_degree_threshold(data: dict, message: Message):
+    # 対象のスレッドを取得
+    target_thread = message.guild.get_channel(TARGET_THREAD_CHANNEL_ID)
+
     # 90度に達した場合の特別なメッセージの処理
     if 'last_reward_date' not in data or data['last_reward_date'] != str(datetime.date.today()):
-        await message.channel.send('90度に達しました！特別なメッセージです！')
+        await target_thread.send('90度に達しました！特別なメッセージです！')
         data['last_reward_date'] = str(datetime.date.today())
     else:
-        await message.channel.send('90度を超えましたが、本日のリワードは受取済みです。')
+        await target_thread.send('90度を超えましたが、本日のリワードは受取済みです。')
     
     reset_temperature(data)
-    await message.channel.send('60度にリセットされました')
+    await target_thread.send('60度にリセットされました')
 
 def reset_temperature(data: dict):
     data['temperature'] = 60
@@ -80,7 +92,7 @@ def load_json():
     except FileNotFoundError:
         # ファイルが存在しない場合は初期データを作成し、新ファイル作成フラグをTrueに設定
         new_file_created = True
-        initial_data = {'temperature': 60, 'message_count': 0, 'yesterday_message_count': 10, 'last_date': str(datetime.date.today()), 'last_reward_date': ''}
+        initial_data = {'temperature': 60, 'message_count': 0, 'yesterday_message_count': 20, 'last_date': str(datetime.date.today()), 'last_reward_date': ''}
         save_json(initial_data)
         return initial_data, new_file_created
 
